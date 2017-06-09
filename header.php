45d0f160151c3584a4d8e4c813b87c8871068c66
Cyrille <cyrille.rieau@gmail.com>
2017-06-09 11:07:23 +0200

    Moif

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 session_start();
 if (isset($_SESSION['user'])){
     echo 'Bonjour '.htmlspecialchars($_SESSION['user']);
+   echo '<form method="POST" action="logout.php">
+<button>Deconnexion</button>
+</form>';
 } else {
     ?>
 <form method="POST" action="inscription.php">
@@ -14,9 +17,7 @@ if (isset($_SESSION['user'])){
 <label>Mot de passe</label><input type="password" name="mdp">
 <button>Connexion</button>
 </form>
-<form method="POST" action="logout.php">
-<button>Deconnexion</button>
-</form>
+
 <?php
 } 
 ?>
